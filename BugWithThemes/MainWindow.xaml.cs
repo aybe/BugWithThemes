930929d413eaf329d48edcd35cd7561c1028c0ef
aybe <aybe@users.noreply.github.com>
2022-03-18 00:40:02 +0100

    Add example

--- BugWithThemes/MainWindow.xaml.cs
+++ BugWithThemes/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using Syncfusion.SfSkinManager;
@@ -15,7 +16,18 @@ public partial class MainWindow
     private void ThemeMenuItem_OnClick(object sender, RoutedEventArgs e)
     {
         // BUG some packages such as Syncfusion.GridCommon.WPF must be installed or some themes will throw
-        var theme = (sender as MenuItem)?.DataContext as ApplicationTheme ?? throw new ArgumentOutOfRangeException(nameof(sender));
+        var theme = (sender as MenuItem)?.DataContext as ApplicationTheme ??
+                    throw new ArgumentOutOfRangeException(nameof(sender));
         SfSkinManager.SetVisualStyle(this, theme.Styles);
+    }
+    
+    private void DockingManager_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        DockingManager1.LoadDockState();
+    }
+
+    private void MainWindow_OnClosing(object? sender, CancelEventArgs e)
+    {
+        DockingManager1.SaveDockState();
     }
 }
